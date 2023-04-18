dec2d39818b43cc930c50cd9225c0c38a7f624b7
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-04-18 19:21:03 -0300

    Serialization & Deserialization

--- protobuf/src/main/java/com/pilgrim/protobuf/PersonDemo.java
+++ protobuf/src/main/java/com/pilgrim/protobuf/PersonDemo.java
@@ -2,17 +2,24 @@ package com.pilgrim.protobuf;
 
 import com.pilgrim.model.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
-        Person person1 = Person.newBuilder()
-                .setName("John")
-                .setAge(20)
-                .build();
-        Person person2 = Person.newBuilder()
-                .setName("john")
-                .setAge(20)
-                .build();
-        System.out.println(person1.equals(person2));
+    public static void main(String[] args) throws IOException {
+//        Person person = Person.newBuilder()
+//                .setName("John")
+//                .setAge(20)
+//                .build();
+
+        Path path = Paths.get("person.ser");
+//        Files.write(path, person.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(path);
+        Person newPerson = Person.parseFrom(bytes);
+        System.out.println(newPerson);
     }
 }
