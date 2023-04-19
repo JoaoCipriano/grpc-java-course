0bfdd2e2169d14f75654ade1d149e84d1cb2967d
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-04-19 19:45:40 -0300

    Collection Demo

--- protobuf/src/main/java/com/pilgrim/protobuf/CompositionDemo.java
+++ protobuf/src/main/java/com/pilgrim/protobuf/CompositionDemo.java
@@ -4,6 +4,9 @@ import com.pilgrim.model.Address;
 import com.pilgrim.model.Car;
 import com.pilgrim.model.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompositionDemo {
 
     public static void main(String[] args) {
@@ -13,17 +16,27 @@ public class CompositionDemo {
                 .setCity("New York")
                 .build();
 
-        Car car = Car.newBuilder()
+        Car accord = Car.newBuilder()
                 .setMake("Honda")
                 .setModel("Accord")
                 .setYear(2023)
                 .build();
 
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2023)
+                .build();
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(accord);
+        cars.add(civic);
+
         Person person = Person.newBuilder()
                 .setName("John")
                 .setAge(20)
                 .setAddress(address)
-                .setCar(car)
+                .addAllCar(cars)
                 .build();
 
         System.out.println(person);
