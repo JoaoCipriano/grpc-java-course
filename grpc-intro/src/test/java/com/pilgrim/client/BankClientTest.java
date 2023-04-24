085e5a5ef9b4fa59c61fbe9e8a5b0f9072e02955
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-04-24 20:04:45 -0300

    Unary - Client's Blocking Stub

---
+++ grpc-intro/src/test/java/com/pilgrim/client/BankClientTest.java
@@ -0,0 +1,37 @@
+package com.pilgrim.client;
+
+import com.pilgrim.model.Balance;
+import com.pilgrim.model.BalanceCheckRequest;
+import com.pilgrim.model.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BankClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    void setUp() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @Test
+    void balanceTest() {
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(5)
+                .build();
+        Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
+        System.out.println(
+                "Received : " + balance.getAmount()
+        );
+        Assertions.assertEquals(50, balance.getAmount());
+    }
+}
