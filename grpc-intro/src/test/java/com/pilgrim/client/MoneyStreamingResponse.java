cf4d73dca29afecbb423c897f4b5b7ff62bc7539
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-06-14 21:11:47 -0300

    Server Streaming - Count Down Latch

--- grpc-intro/src/test/java/com/pilgrim/client/MoneyStreamingResponse.java
+++ grpc-intro/src/test/java/com/pilgrim/client/MoneyStreamingResponse.java
@@ -3,7 +3,15 @@ package com.pilgrim.client;
 import com.pilgrim.model.Money;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MoneyStreamingResponse implements StreamObserver<Money> {
+
+    private final CountDownLatch latch;
+
+    public MoneyStreamingResponse(CountDownLatch latch) {
+        this.latch = latch;
+    }
 
     @Override
     public void onNext(Money money) {
@@ -13,10 +21,12 @@ public class MoneyStreamingResponse implements StreamObserver<Money> {
     @Override
     public void onError(Throwable throwable) {
         System.out.println(throwable.getMessage());
+        latch.countDown();
     }
 
     @Override
     public void onCompleted() {
         System.out.println("Server is done!!");
+        latch.countDown();
     }
 }
