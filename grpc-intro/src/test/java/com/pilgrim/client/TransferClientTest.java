2282d2caf90da13ef4a33ee62041d4e082b756fc
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-07-31 20:03:44 -0300

    BiDi - Client Side Stub Request and Response

---
+++ grpc-intro/src/test/java/com/pilgrim/client/TransferClientTest.java
@@ -0,0 +1,45 @@
+package com.pilgrim.client;
+
+import com.pilgrim.model.TransferRequest;
+import com.pilgrim.model.TransferServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TransferClientTest {
+
+    private TransferServiceGrpc.TransferServiceStub stub;
+
+    @BeforeAll
+    void setUp() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        stub = TransferServiceGrpc.newStub(managedChannel);
+    }
+
+    @Test
+    void transfer() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        TransferStreamingResponse response = new TransferStreamingResponse(latch);
+        StreamObserver<TransferRequest> requestStreamObserver = this.stub.transfer(response);
+
+        for (int i = 0; i < 100; i++) {
+            var request = TransferRequest.newBuilder()
+                    .setFromAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setToAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setAmount(ThreadLocalRandom.current().nextInt(1, 21))
+                    .build();
+            requestStreamObserver.onNext(request);
+        }
+        requestStreamObserver.onCompleted();
+        latch.await();
+    }
+}
