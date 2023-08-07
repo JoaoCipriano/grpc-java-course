b0855850c987521df36f64ab5b346f0fe6890f95
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-07 19:07:52 -0300

    Load Balancing - Nginx Loading Balancing Demo - Part II

--- grpc-intro/src/test/java/com/pilgrim/client/loadbalancing/NginxClientTest.java
+++ grpc-intro/src/test/java/com/pilgrim/client/loadbalancing/NginxClientTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class NginxClientTest {
 
@@ -25,15 +27,21 @@ class NginxClientTest {
 
     @Test
     void balanceTest() {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
+            int randomInt = ThreadLocalRandom.current().nextInt(1, 10);
             BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
-                    .setAccountNumber(i)
+                    .setAccountNumber(randomInt)
                     .build();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
             System.out.println(
                     "Received : " + balance.getAmount()
             );
-            Assertions.assertEquals(i * 100, balance.getAmount());
+            Assertions.assertEquals(randomInt * 100, balance.getAmount());
         }
     }
 }
