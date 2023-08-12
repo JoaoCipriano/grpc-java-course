785a21575112bef1691ffc46b5d4ad431ca3c27f
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-12 10:53:23 -0300

    Interceptor - Client Service Token Validation - Implementation

---
+++ grpc-intro/src/main/java/com/pilgrim/server/metadata/AuthInterceptor.java
@@ -0,0 +1,28 @@
+package com.pilgrim.server.metadata;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import io.grpc.Status;
+
+import java.util.Objects;
+
+public class AuthInterceptor implements ServerInterceptor {
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
+        String clientToken = metadata.get(ServerConstants.TOKEN);
+        if (this.validate(clientToken)) {
+            return serverCallHandler.startCall(serverCall, metadata);
+        } else {
+            Status status = Status.UNAUTHENTICATED.withDescription("invalid token/expired token");
+            serverCall.close(status, metadata);
+        }
+        return new ServerCall.Listener<>() {
+        };
+    }
+
+    private boolean validate(String token) {
+        return Objects.nonNull(token) && token.equals("bank-client-secret");
+    }
+}
