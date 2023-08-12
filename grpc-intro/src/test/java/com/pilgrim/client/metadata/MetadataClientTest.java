5692cd6ea051fad38257c93ca45f5d05e2741bf8
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-12 10:40:10 -0300

    Interceptor - Attaching Client Service Token Via Metadata

---
+++ grpc-intro/src/test/java/com/pilgrim/client/metadata/MetadataClientTest.java
@@ -0,0 +1,85 @@
+package com.pilgrim.client.metadata;
+
+import com.pilgrim.client.deadline.DeadlineInterceptor;
+import com.pilgrim.client.rpctypes.MoneyStreamingResponse;
+import com.pilgrim.model.Balance;
+import com.pilgrim.model.BalanceCheckRequest;
+import com.pilgrim.model.BankServiceGrpc;
+import com.pilgrim.model.WithdrawRequest;
+import io.grpc.Deadline;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.MetadataUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MetadataClientTest {
+
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private BankServiceGrpc.BankServiceStub bankServiceStub;
+
+    @BeforeAll
+    void setUp() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .intercept(MetadataUtils.newAttachHeadersInterceptor(ClientConstants.getClientToken()))
+                .usePlaintext()
+                .build();
+        blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+        bankServiceStub = BankServiceGrpc.newStub(managedChannel);
+    }
+
+    @Test
+    void balanceTest() {
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(7)
+                .build();
+        try {
+            Balance balance = this.blockingStub
+                    .getBalance(balanceCheckRequest);
+            System.out.println(
+                    "Received : " + balance.getAmount()
+            );
+            Assertions.assertEquals(700, balance.getAmount());
+        } catch (StatusRuntimeException exception) {
+            // go with default values
+        }
+    }
+
+    @Test
+    void withdrawTest() {
+        var withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(6)
+                .setAmount(50)
+                .build();
+
+        try {
+            this.blockingStub
+                    .withDeadline(Deadline.after(2, TimeUnit.SECONDS))
+                    .withdraw(withdrawRequest)
+                    .forEachRemaining(money -> System.out.println("Received : " + money.getValue()));
+
+        } catch (StatusRuntimeException exception) {
+            //
+        }
+    }
+
+    @Test
+    void withdrawAsyncTest() {
+        CountDownLatch latch = new CountDownLatch(1);
+        var withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(7)
+                .setAmount(40)
+                .build();
+        Assertions.assertDoesNotThrow(() -> {
+            this.bankServiceStub.withdraw(withdrawRequest, new MoneyStreamingResponse(latch));
+            latch.await();
+        });
+    }
+}
