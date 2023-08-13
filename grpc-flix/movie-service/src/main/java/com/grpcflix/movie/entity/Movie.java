daedb38b85faf0e98cbd88ea6d18b85fbbf9a6c4
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-13 20:10:08 -0300

    Spring Boot Integration - Movie Service - Implementation

---
+++ grpc-flix/movie-service/src/main/java/com/grpcflix/movie/entity/Movie.java
@@ -0,0 +1,28 @@
+package com.grpcflix.movie.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private int id;
+    private String title;
+    private int year;
+    private double rating;
+    private String genre;
+}
