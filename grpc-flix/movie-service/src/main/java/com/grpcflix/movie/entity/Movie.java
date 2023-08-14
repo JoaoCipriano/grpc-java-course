6ab2cc804ac6230f756b9154fc1a3856af430644
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-13 21:24:34 -0300

    Add some fixes

--- grpc-flix/movie-service/src/main/java/com/grpcflix/movie/entity/Movie.java
+++ grpc-flix/movie-service/src/main/java/com/grpcflix/movie/entity/Movie.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -22,6 +23,7 @@ public class Movie {
     @Id
     private int id;
     private String title;
+    @Column(name = "release_year")
     private int year;
     private double rating;
     private String genre;
