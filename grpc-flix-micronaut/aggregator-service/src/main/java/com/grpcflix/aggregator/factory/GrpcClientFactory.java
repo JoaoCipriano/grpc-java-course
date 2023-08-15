5b4423849088f1780880f14b0c58c6ec3a50cebb
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-08-15 16:48:46 -0300

    Micronaut Integration With gRPC + SSL/TLS

--- grpc-flix-micronaut/aggregator-service/src/main/java/com/grpcflix/aggregator/factory/GrpcClientFactory.java
+++ grpc-flix-micronaut/aggregator-service/src/main/java/com/grpcflix/aggregator/factory/GrpcClientFactory.java
@@ -3,9 +3,19 @@ package com.grpcflix.aggregator.factory;
 import com.pilgrim.grpcflix.movie.MovieServiceGrpc;
 import com.pilgrim.grpcflix.user.UserServiceGrpc;
 import io.grpc.ManagedChannel;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.grpc.annotation.GrpcChannel;
 import jakarta.inject.Singleton;
+
+import javax.net.ssl.SSLException;
+import javax.xml.crypto.URIReferenceException;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Optional;
 
 @Factory
 public class GrpcClientFactory {
@@ -17,8 +27,17 @@ public class GrpcClientFactory {
     }
 
     @Singleton
-    public MovieServiceGrpc.MovieServiceBlockingStub movieClientStub(@GrpcChannel("movie-service") ManagedChannel channel) {
+    public MovieServiceGrpc.MovieServiceBlockingStub movieClientStub() throws SSLException, URISyntaxException, URIReferenceException {
+        URL certificationUrl = Optional.ofNullable(this.getClass().getClassLoader().getResource("certs/ca.cert.pem"))
+                .orElseThrow(URIReferenceException::new);
+        SslContext sslContext = GrpcSslContexts.forClient()
+                .trustManager(new File(certificationUrl.toURI()))
+                .build();
+        ManagedChannel managedChannel = NettyChannelBuilder.forAddress("localhost", 7575)
+                .sslContext(sslContext)
+                .build();
+
         return MovieServiceGrpc
-                .newBlockingStub(channel);
+                .newBlockingStub(managedChannel);
     }
 }
